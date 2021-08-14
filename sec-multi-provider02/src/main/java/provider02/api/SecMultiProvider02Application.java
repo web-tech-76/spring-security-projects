ebf1a9d479661381905b713034623950ff6c1444
web_tech_76 <webtec76@gmail.com>
2021-08-14 14:13:31 +0530

    initial commit for sec-multi-pro-02

---
+++ sec-multi-provider02/src/main/java/provider02/api/SecMultiProvider02Application.java
@@ -0,0 +1,13 @@
+package provider02.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecMultiProvider02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecMultiProvider02Application.class, args);
+    }
+
+}
---
+++ sec-multi-provider02/src/test/java/provider02/api/SecMultiProvider02ApplicationTests.java
@@ -0,0 +1,13 @@
+package provider02.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecMultiProvider02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
